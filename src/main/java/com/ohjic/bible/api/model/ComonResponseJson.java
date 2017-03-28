ce12ccb3eb6cb3a6ab2e281c7e34c5408c3dd095
gusfot <hyunlae.kim@ohjic.com>
2017-03-28 09:00:27 +0900

    공통 restful 응답 객체

---
+++ src/main/java/com/ohjic/bible/api/model/ComonResponseJson.java
@@ -0,0 +1,45 @@
+package com.ohjic.bible.api.model;
+
+/**
+ * Created by gusfot
+ */
+public class ComonResponseJson {
+    private String status;
+    private String msg;
+    private Object data;
+
+    public ComonResponseJson(String status, String msg, Object data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public ComonResponseJson(String status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
